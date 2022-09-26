082345f3747874c67e36c362f1800fecb5492e36
Eshaan Mittal <eshaan.mittal1@gmail.com>
2022-09-26 21:47:46 +0200

    q's

---
+++ src/EnRectangle.java
@@ -0,0 +1,12 @@
+/**
+ * EnRectangle.java
+ * Question 9
+ * @author eshaan
+ */
+import java.util.Scanner;
+
+public class EnRectangle {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Maxof2Amb2if.java
@@ -0,0 +1,22 @@
+/**
+ * Maxof2Amb2if.java
+ * Question 8
+ * @author eshaan
+ */
+import java.util.Scanner;
+
+public class Maxof2Amb2if {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        num1 = sc.nextInt(); sc.nextLine();
+        System.out.print("Enter second number: ");
+        num2 = sc.nextInt(); sc.nextLine();
+        System.out.print("Enter third number: ");
+        num3 = sc.nextInt(); sc.nextLine();
+
+        //Stupid code
+    }
+}
