40ceb53c33bc1c6302c748eecc205fc0db8e9d7a
Eshaan Mittal <eshaan.mittal1@gmail.com>
2022-09-28 00:01:48 +0200

    q's

--- src/EnRectangle.java
+++ src/EnRectangle.java
@@ -6,23 +6,23 @@
 import java.util.Scanner;
 public class EnRectangle {
     public static void main(String[] args) {
-        int x1, y1;  //Bottom left
-        int x2, y2;  //Top right
-        int x, y;    //Point to check
+        float x1, y1;  //Bottom left
+        float x2, y2;  //Top right
+        float x, y;    //Point to check
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter x of bottom left: ");
-        x1 = sc.nextInt(); sc.nextLine();
+        x1 = sc.nextFloat(); sc.nextLine();
         System.out.print("Enter y of bottom left: ");
-        y1 = sc.nextInt(); sc.nextLine();
+        y1 = sc.nextFloat(); sc.nextLine();
         System.out.print("Enter x of top right: ");
-        x2 = sc.nextInt(); sc.nextLine();
+        x2 = sc.nextFloat(); sc.nextLine();
         System.out.print("Enter y of top right: ");
-        y2 = sc.nextInt(); sc.nextLine();
+        y2 = sc.nextFloat(); sc.nextLine();
         System.out.print("Enter x of new point: ");
-        x = sc.nextInt(); sc.nextLine();
+        x = sc.nextFloat(); sc.nextLine();
         System.out.print("Enter y of new point: ");
-        y = sc.nextInt(); sc.nextLine();
+        y = sc.nextFloat(); sc.nextLine();
 
         if(x >= x1 && x <= x2 && y >= y1 && y <= y2)
             System.out.println("Point is inside the rectangle");
---
+++ src/MinRectangle.java
@@ -0,0 +1,45 @@
+/**
+ * MinRectangle.java
+ * Question 10
+ * @author eshaan
+ */
+import java.util.Scanner;
+
+public class MinRectangle {
+    public static void main(String[] args) {
+        //First rectangle
+        float r1bx, r1by;  //Bottom left
+        float r1tx, r1ty;  //Top right
+        //Second rectangle
+        float r2bx, r2by;  //Bottom left
+        float r2tx, r2ty;  //Top right
+        Scanner sc = new Scanner(System.in);
+        boolean intersection = false;
+        //First rectangle coordinates
+        System.out.print("Enter x of bottom left (Rectangle 1): ");
+        r1bx = sc.nextFloat(); sc.nextLine();
+        System.out.print("Enter y of bottom left (Rectangle 1): ");
+        r1by = sc.nextFloat(); sc.nextLine();
+        System.out.print("Enter x of top right (Rectangle 1): ");
+        r1tx = sc.nextFloat(); sc.nextLine();
+        System.out.print("Enter y of top right (Rectangle 1): ");
+        r1ty = sc.nextFloat(); sc.nextLine();
+        //Second rectangle coordinates
+        System.out.print("Enter x of bottom left (Rectangle 2): ");
+        r2bx = sc.nextFloat(); sc.nextLine();
+        System.out.print("Enter y of bottom left (Rectangle 2): ");
+        r2by = sc.nextFloat(); sc.nextLine();
+        System.out.print("Enter x of top right (Rectangle 2): ");
+        r2tx = sc.nextFloat(); sc.nextLine();
+        System.out.print("Enter y of top right (Rectangle 2): ");
+        r2ty = sc.nextFloat(); sc.nextLine();
+
+        if(((r1bx <= r2tx && r1bx >= r2bx) || (r1tx <= r2tx && r1tx >= r2bx)) && (r1by <= r2ty && r1by >= r2by) || (r1ty <= r2ty && r1ty >= r2by))
+            intersection = true;
+
+        if(intersection) {
+            //TODO
+        } else
+            System.out.println("No intersection so no minimum rectangle");
+    }
+}
