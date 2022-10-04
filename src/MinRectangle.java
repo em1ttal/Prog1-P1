208085258cd535ff8fb0b747f18a9972caf1b337
Eshaan Mittal <eshaan.mittal1@gmail.com>
2022-10-04 19:57:49 +0200

    q's

--- src/MinRectangle.java
+++ src/MinRectangle.java
@@ -10,11 +10,14 @@ public class MinRectangle {
         //First rectangle
         float r1bx, r1by;  //Bottom left
         float r1tx, r1ty;  //Top right
+
         //Second rectangle
         float r2bx, r2by;  //Bottom left
         float r2tx, r2ty;  //Top right
+
         Scanner sc = new Scanner(System.in);
-        boolean intersection = false;
+        boolean intersection = true;
+
         //First rectangle coordinates
         System.out.print("Enter x of bottom left (Rectangle 1): ");
         r1bx = sc.nextFloat(); sc.nextLine();
@@ -24,6 +27,7 @@ public class MinRectangle {
         r1tx = sc.nextFloat(); sc.nextLine();
         System.out.print("Enter y of top right (Rectangle 1): ");
         r1ty = sc.nextFloat(); sc.nextLine();
+
         //Second rectangle coordinates
         System.out.print("Enter x of bottom left (Rectangle 2): ");
         r2bx = sc.nextFloat(); sc.nextLine();
@@ -34,12 +38,17 @@ public class MinRectangle {
         System.out.print("Enter y of top right (Rectangle 2): ");
         r2ty = sc.nextFloat(); sc.nextLine();
 
-        if (((r1bx <= r2tx && r1bx >= r2bx) || (r1tx <= r2tx && r1tx >= r2bx))
-        && ((r1by <= r2ty && r1by >= r2by) || (r1ty <= r2ty && r1ty >= r2by)))
-            intersection = true;
+        if (r2ty < r1by || r1tx < r2bx || r1ty < r2by || r2tx < r1bx)
+            intersection = false;
 
         if(intersection) {
-            //TODO
+            float intersect_bx = Math.max(r1bx, r2bx);
+            float intersect_by = Math.max(r1by, r2by);
+            float intersect_tx = Math.min(r1tx, r2tx);
+            float intersect_ty = Math.min(r1ty, r2ty);
+            System.out.println("Coordinates of intersection are: ");
+            System.out.println("Bottom Left: (" + intersect_bx + ", " + intersect_by + ")");
+            System.out.println("Top Right: (" + intersect_tx + ", " + intersect_ty + ")");
         } else
             System.out.println("No intersection, no minimum rectangle");
     }
